9360d2057f9d8d35803a518ec09decaeab8dfaa7
ChristianHopf <cehopf@gmail.com>
2025-02-23 11:06:16 -0500

    Header for client functions

---
+++ client.h
@@ -0,0 +1,20 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+/* Connects to an IRC server
+ * Returns socket file descriptor (-1 on error)
+ */
+int connect_to_server(const char *ip);
+
+/* Sends an IRC command via the socket sockfd
+ * command: string representing command type (ex. NICK, USER, etc)
+ * text: text of the command (ex. text after 'USER' in 'USER bruh * * :Bro Brah')
+ */
+int send_command(int sockfd, const char *command, const char *text);
+
+/* Polls for data in the socket sockfd via poll(),
+ * then reads data into buffer
+ */
+int poll_and_read(int sokcfd, const char *buffer, size_t buf_size);
+
+#endif
